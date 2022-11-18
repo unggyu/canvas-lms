fbc0233d46597bf0f0cf33f39ba3b8b28108740d
unggyu <cdr2116@xinics.com>
2022-11-18 08:57:59 +0000

    Feat: PTT3-371 모듈 > 파일 선택 시 Synap 뷰어 표시되도록 함

--- app/helpers/attachment_helper.rb
+++ app/helpers/attachment_helper.rb
@@ -35,7 +35,7 @@ module AttachmentHelper
       rescue => e
         Canvas::Errors.capture_exception(:crocodoc, e)
       end
-    elsif attachment.canvadocable?
+    elsif attachment.custom_previewable? || attachment.canvadocable?
       attrs[:canvadoc_session_url] = attachment.canvadoc_url(@current_user, url_opts)
     end
     attrs[:attachment_id] = attachment.id
